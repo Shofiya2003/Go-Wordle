cd416e3198cb1f1b7bb0d9dd7d020ac975fb329a
Shofiya2003 <shofiyabootwala@gmail.com>
2022-08-18 03:01:25 +0530

    added a function to check with the correct word and printed the grid of letters with colors according to the status of the letters

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fatih/color"
 )
 
 var letters [25]int
+var wordOfDay = "goods"
+var userLetters []int
 
 func main() {
 	var word string
@@ -21,23 +24,75 @@ func main() {
 		}
 	}
 	getLetters(word, &letters)
-	printGrid()
+	printGrid(letters)
 }
 
 func getLetters(word string, letters *[25]int) {
 	chars := []rune(word)
+	compareResponseWithWord(word, letters)
 	for _, char := range chars {
 		charValue := int(char)
-		letters[charValue-97] = 1
+		userLetters = append(userLetters, charValue)
 	}
 }
 
-func printGrid() {
+func compareResponseWithWord(word string, letters *[25]int) {
+	chars := []rune(word)
+	for index, char := range chars {
+		charValue := int(char)
+		contains := strings.Contains(wordOfDay, string(char))
+		isAtRightIndex := false
+		if contains {
+			isAtRightIndex = checkIfAtRightIndex(string(char), index)
+		}
+		if isAtRightIndex {
+			letters[charValue-97] = 1
+		} else if contains {
+			letters[charValue-97] = 2
+		} else {
+			letters[charValue-97] = -1
+		}
+	}
+}
+
+func checkIfAtRightIndex(char string, requiredIndex int) (res bool) {
+	chars := []rune(wordOfDay)
+	for index, element := range chars {
+		if string(element) == char && requiredIndex == index {
+			return true
+		}
+	}
+	return false
+}
+
+func printResponseGrid() {
 	count := 1
-	for index, status := range letters {
-		char := string(index + 97)
+	for _, element := range userLetters {
+		char := string(element)
+		status := letters[element-97]
 		if status == 1 {
 			fmt.Printf("%s  ", color.YellowString(char))
+		} else {
+			fmt.Printf("%s  ", char)
+		}
+		if count == 5 {
+			fmt.Println()
+			count = 0
+		}
+		count += 1
+	}
+}
+
+func printGrid(grid [25]int) {
+	count := 1
+	for index, status := range grid {
+		char := string(index + 97)
+		if status == 1 {
+			fmt.Printf("%s  ", color.GreenString(char))
+		} else if status == 2 {
+			fmt.Printf("%s  ", color.YellowString(char))
+		} else if status == -1 {
+			fmt.Printf("%s  ", color.BlueString(char))
 		} else {
 			fmt.Printf("%s  ", char)
 		}
